9eb3e9127dd12154314703095d78b53e391fd29c
Kate Corcoran <kate.corcoran@dfstudio.com>
2024-11-25 18:09:21 -0800

    Update dist

--- dist/vue-class-store.esm.js
+++ dist/vue-class-store.esm.js
@@ -1,6 +1,6 @@
 /**
  * Bundle of: vue-class-store
- * Generated: 2024-08-26
+ * Generated: 2024-11-25
  * Version: 3.0.0
  */
 
@@ -25,8 +25,9 @@ function getValue(value, path) {
     return value;
 }
 // 'on.flag:target', 'on.flag1.flag2:target'
-// flags: deep, immediate, pre, post, sync
-let watchPattern = /^on(\.[.a-zA-Z]*)?:(.*)$/;
+// 'on.deep=1.immediate:target'
+// flags: deep, deep=N, immediate, once, pre, post, sync
+let watchPattern = /^on(\.[.a-zA-Z0-9=]*)?:(.*)$/;
 function isWatch(key) {
     return watchPattern.test(key);
 }
@@ -34,11 +35,15 @@ function parseWatch(key) {
     var _a;
     let match = key.match(watchPattern);
     let flags = (_a = match[1]) !== null && _a !== void 0 ? _a : '';
+    let deepMatch = flags.match(/\.deep(?:=(\d+))?/);
     return {
         path: match[2].split('.'),
         options: {
-            deep: flags.includes('.deep'),
+            deep: deepMatch && deepMatch[1] ? parseInt(deepMatch[1]) :
+                deepMatch ? true :
+                    undefined,
             immediate: flags.includes('.immediate'),
+            once: flags.includes('.once'),
             flush: flags.includes('.pre') ? 'pre'
                 : flags.includes('.post') ? 'post'
                     : flags.includes('.sync') ? 'sync'
@@ -62,17 +67,66 @@ function addComputed(instance, descriptors) {
         }
     });
 }
+const warned = new Set();
+const COMPAT_WARN_MSG = `watches on an array value will no longer trigger on array mutation unless the ".deep" flag is \
+specified. You can specify the intended behavior and suppress this warning by setting an explicit ".deep" flag:
+
+  "on.deep=0:yourArray"(newValue, oldValue) { // Vue 3 semantics, doesn't trigger on mutation
+  "on.deep=1:yourArray"(newValue, oldValue) { // Vue 2 semantics, triggers on mutation
+
+  Details: https://v3-migration.vuejs.org/breaking-changes/watch.html`;
+function warnCompat(className, propertyKey) {
+    if (!warned.has(`${className}%${propertyKey}`)) {
+        warned.add(`${className}%${propertyKey}`);
+        // Same format as the Vue compat build
+        let message = '[Vue warn]: (deprecation WATCH_ARRAY @ vue-class-store) ';
+        if (warned.size == 1) {
+            message += COMPAT_WARN_MSG; // only print the full message once
+        }
+        else {
+            message += `(${warned.size})`;
+        }
+        message += `\n  at ${className}["${propertyKey}"]`;
+        console.warn(message);
+    }
+}
+// https://github.com/vuejs/core/pull/12236
+function compatWatch(className, propertyKey, source, callback, options) {
+    if (options.deep != undefined) {
+        return watch(source, callback, options); // if deep is specified, even `deep: 0`, don't activate the compat
+    }
+    else {
+        return watch(() => {
+            const value = source();
+            if (Array.isArray(value)) {
+                warnCompat(className, propertyKey);
+                value.slice(0, 0); // establish reactive dependency on array iteration order
+                return { COMPAT_ARRAY_UNWRAP: value };
+            }
+            return value;
+        }, (newValue, oldValue, onCleanup) => {
+            if (newValue && newValue.COMPAT_ARRAY_UNWRAP) {
+                newValue = newValue.COMPAT_ARRAY_UNWRAP;
+            }
+            if (oldValue && oldValue.COMPAT_ARRAY_UNWRAP) {
+                oldValue = oldValue.COMPAT_ARRAY_UNWRAP;
+            }
+            return callback(newValue, oldValue, onCleanup);
+        }, options);
+    }
+}
 /**
  * Scans the model for `on:*` watchers and then creates watches for them. This method expects to be passed a reactive
  * model.
  */
 function addWatches(instance, descriptors) {
     descriptors.forEach(([key, desc]) => {
+        var _a, _b;
         if (isWatch(key)) {
             let { path, options } = parseWatch(key);
             let callback = typeof desc.value === 'string' ? instance[desc.value] : desc.value;
             if (typeof callback === 'function') {
-                watch(() => getValue(instance, path), callback.bind(instance), options);
+                compatWatch((_b = (_a = instance.constructor) === null || _a === void 0 ? void 0 : _a.name) !== null && _b !== void 0 ? _b : 'Unknown', key, () => getValue(instance, path), callback.bind(instance), options);
             }
         }
     });
